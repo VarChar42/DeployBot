39f7963590dbab0670d28df83d88e58fe095586a
Michael <michw2014@gmail.com>
2020-09-14 12:02:18 +0200

    Updated deploybot

--- index.py
+++ index.py
@@ -3,14 +3,19 @@ import wget
 import requests 
 import zipfile
 import shutil
+import stat
+import platform
 
-target_folder = "."
-repos = ['VortexdataNET/vortexdata.net', 'VarChar42/filerift-windows']
+target_folder = '.'
+deployed_folder_permissions = ''
+repos = ['VortexdataNET/vortexdata.net', 'VarChar42/filerift-windows', 'VortexdataNET/src.vortexdata.net', 'VarChar42/vortexdata.net']
+
+def log(msg):
+	print('\t | %s' % msg)
 
 
 token = open('token.cfg', 'r').readline()
 
-print(token)
 gh_session = requests.Session()
 gh_session.auth = ('VarChar42', token)
 
@@ -24,39 +29,64 @@ for repo in repos:
 
 	result = gh_session.get(url = 'https://api.github.com/repos/%s/releases/latest' % repo).json()
 	if 'message' in result and result['message'] == 'Not Found':
-		print ('\t | Cant find release .. Skipping')
+		log('Cant find release .. Skipping')
 		continue
 	
 	if not 'name' in result:
-		print ('\t | Something went wrong .. Skipping')
-		print ('\t | Received message: %s' % result)
+		log('Something went wrong .. Skipping')
+		log('Received message: %s' % result)
 		continue
 	
-	dl_url 	= result['zipball_url']
+	log('Release found: %s' % result['name'])
+	
+	assets = result['assets']
+	
+	if len(assets) < 1:
+		log('Release has no assets .. Skipping')
+		continue
+		
+	asset = assets[0]
+	log('Asset found: %s' % asset['name'])
+	
+	if not asset['name'].endswith('.zip'):
+		log('Asset is not a zip file .. Skipping')
+		continue
+	
+	dl_url 	= asset['url']
 	dl_file = 'temp/release_%s.zip' % result['id']
 	
-	print("\t | Release found: %s" % result['name'])
-	print("\t | Starting download ...")
-	dl_data = gh_session.get(dl_url)
+
+	log('Starting download ...')
+	
+	dl_data = gh_session.get(dl_url, headers={'Accept': 'application/octet-stream'})
 	
 	with open(dl_file, 'wb') as file:
 		file.write(dl_data.content)
 	
-	print("\t | Saved as: %s" % dl_file)
-	print("\t | Extracting zipball ...")
+	log('Saved as: %s' % dl_file)
+	log('Extracting archive ...')
 	
 	with zipfile.ZipFile(dl_file, 'r') as zip_ref:
 		zip_ref.extractall('temp')
 		repo_folder = zip_ref.namelist()[0]
 		
 		
-	print("\t | Moving release into target folder ...")
-	shutil.move('temp/%s' % repo_folder, '%s/%s' % (target_folder, name))
+	log('Moving release into target folder ...')
 	
-	print("\t | Cleanup ... ")
+	if os.path.exists(name):
+		log('Deleting existing folder ...')
+		shutil.rmtree(name)
+	shutil.move('temp/%s' % repo_folder, '%s/%s/' % (target_folder, name))
+	
+	if platform.system() == 'Linux':
+		log('Applying file permissions ...')
+		os.system('chmod -R %s %s', (deployed_folder_permissions, name))
+	
+	log('Cleanup ... ')
 	
 	os.remove(dl_file)
 	
-	print("\t | Done!")
-	
+	log('Done!')
+
+
 	
